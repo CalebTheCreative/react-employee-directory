c08ccfabe89f8856253a8b263bb6cab72da9f80b
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-16 15:00:00 -0500

    Added Jumbotron component.

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Jumbotron() {
+    return(
+
+        <div className="jumbotron">
+            Employee Directory
+        </div>
+
+    );
+};
+
+export default Jumbotron;
