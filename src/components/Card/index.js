6df55f50b9346bfa9d9d2af7640d4fc98d931f64
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-17 04:06:20 -0500

    Added sorting functionality.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 function Card(props) {
-    return(
-        <tr>
-            <th><img src={props.picture} alt={props.firstName}/></th>
-            <td>{props.firstName}</td>
-            <td>{props.lastName}</td>
-            <td>{props.country}</td>
-            <td>{props.email}</td>
-            <td>{props.phone}</td>
-        </tr>
+    return (
+        <tbody>
+            <tr>
+                <td><img src={props.picture} alt={props.firstName} /></td>
+                <td>{props.firstName}</td>
+                <td>{props.lastName}</td>
+                <td>{props.country}</td>
+                <td>{props.email}</td>
+                <td>{props.phone}</td>
+            </tr>
+        </tbody>
     );
 };
 
--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -6,10 +6,18 @@ import SearchContainer from "../SearchContainer";
 class Results extends Component {
     state = {
         result: [],
-        searchRes: ""
+        searchRes: "",
+        abc: true,
+        ascending: true,
+        empSorted: []
     };
 
     componentDidMount() {
+        if (this.state.empSorted.length < 1) {
+            this.setState({
+                empSorted: this.state.result
+            })
+        }
         this.searchEmps();
     }
 
@@ -48,6 +56,7 @@ class Results extends Component {
     }
 
     filterEmps = (id) => {
+        // eslint-disable-next-line array-callback-return
         const filteredEmps = this.state.result.filter(employee => {
             if (employee.lastName === id) {
                 return employee.lastName;
@@ -56,17 +65,50 @@ class Results extends Component {
                 return employee.firstName;
             }
         });
-            console.log("ID");
-            console.log(id);
-            console.log("---");
-            this.setState({
-                result: filteredEmps
-            });
+        console.log("ID");
+        console.log(id);
+        console.log("---");
+        this.setState({
+            result: filteredEmps
+        });
+    }
+
+    handleSortLast = () => {
+        let sortedResults = [];
+        
+        if (this.state.abc){
+            sortedResults = this.state.result.sort((a,b) => {
+                let lowerA = a.lastName.toLowerCase();
+                let lowerB = b.lastName.toLowerCase();
+                if (lowerA < lowerB){
+                    return -1
+                }
+                if (lowerA > lowerB){
+                    return 1
+                }
+                return 0
+            })
+        } else {
+            sortedResults = this.state.result.sort((a, b) => {
+                let lowerA = a.lastName.toLowerCase();
+                let lowerB = b.lastName.toLowerCase();
+                if (lowerA > lowerB)
+                    return -1
+                if (lowerA < lowerB)
+                    return 1
+                return 0
+            })
         }
+        this.setState({
+            abc: !this.state.abc,
+            empSorted: sortedResults
+        })
+    }
 
     render() {
-            return(
-            <div className = "container" >
+        return (
+            <div className="container" >
+
                 <div className="row justify-content-center">
                     <SearchContainer
                         value={this.state.searchRes}
@@ -74,31 +116,34 @@ class Results extends Component {
                         handleFormSubmit={this.handleFormSubmit}
                     />
                 </div>
+                {/* <div className="row justify-content-center">
+                    <button className="btn sortLast" onClick={this.handleSortLast}>Sort by Last Name</button>
+                </div> */}
                 <div className="row justify-content-center">
 
-                    <table>
-                        <tbody>
+                    <table className="table">
+                        <thead className="thead-light">
                             <tr>
-                                <th>Photo</th>
-                                <td>First Name</td>
-                                <td>Last Name</td>
-                                <td>Country</td>
-                                <td>Email</td>
-                                <td>Phone</td>
+                                <th scope="col">Photo</th>
+                                <th scope="col">First Name</th>
+                                <th scope="col" onClick={this.handleSortLast}>Last Name</th>
+                                <th scope="col">Country</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Phone</th>
                             </tr>
+                        </thead>
+                        {[...this.state.result].map((i) =>
+                            <Card
+                                picture={i.picture}
+                                firstName={i.firstName}
+                                lastName={i.lastName}
+                                country={i.country}
+                                email={i.email}
+                                phone={i.phone}
+                                key={i.key}
+                            />
+                        )}
 
-                            {[...this.state.result].map((i) =>
-                                <Card
-                                    picture={i.picture}
-                                    firstName={i.firstName}
-                                    lastName={i.lastName}
-                                    country={i.country}
-                                    email={i.email}
-                                    phone={i.phone}
-                                    key={i.key}
-                                />
-                            )}
-                        </tbody>
                     </table>
                 </div>
             </div >
